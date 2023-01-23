0ccbd60963983b40e248d7674866d6c9d687f65a
rshtepin <shtep@list.ru>
2023-01-23 08:51:17 +0500

    update

--- src/products/components/AllProducts.tsx
+++ src/products/components/AllProducts.tsx
@@ -5,6 +5,8 @@ import { usePagination } from 'src/core/hooks/usePagination'
 import { Routes } from '@blitzjs/next'
 import ProductItem from './ProductItem'
 import { Wrap, WrapItem } from '@chakra-ui/react'
+import { useSession } from '@blitzjs/auth'
+import { Button } from '@chakra-ui/react'
 const ITEMS_PER_PAGE = 30
 const deleteProduct = (item) => {
   console.log(item)
@@ -16,23 +18,43 @@ const GetU = () => {
     skip: ITEMS_PER_PAGE * pagination.page,
     take: ITEMS_PER_PAGE,
   })
+
   console.log(products)
   return (
     <>
-      {products.map((prod) => (
-        <WrapItem key={prod.id}>
-          <ProductItem key={prod.id} product={prod} onDelete={deleteProduct} />
+      {products.map((item) => (
+        <WrapItem key={item.id}>
+          <ProductItem key={item.id} product={item} onDelete={deleteProduct} />
         </WrapItem>
       ))}
     </>
   )
 }
+// Блок администратора
+const AdminBlock = (product, onDelete) => {
+  const session = useSession()
+  const userId = session.userId
+  const role = session.role
+
+  if (role == 'ADMIN')
+    return (
+      <>
+        <Button colorScheme="yellow">Редактор шаблона продукта</Button>
+        <Button colorScheme="red" onClick={() => onDelete(product.id)}>
+          Добавить продукт
+        </Button>
+      </>
+    )
+}
+///////////////////
+
 const AllProducts = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Wrap spacing="30px">
         <GetU />
       </Wrap>
+      <AdminBlock />
     </Suspense>
   )
 }
