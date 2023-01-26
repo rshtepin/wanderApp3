a3c04bd6a7007453d385062e28204bfd3ec0f151
rshtepin <shtep@list.ru>
2023-01-27 01:58:21 +0500

    image width

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,21 +1,27 @@
-import { useState } from 'react'
+import { createRef, useState } from 'react'
 
 export default function PrivatePage(props) {
   const [image, setImage] = useState(null)
   const [createObjectURL, setCreateObjectURL] = useState(null)
+  const imgRef = createRef()
 
-  const uploadToClient = (event) => {
+  const uploadToClient = async (event) => {
     if (event.target.files && event.target.files[0]) {
       const i = event.target.files[0]
-
       setImage(i)
+
       setCreateObjectURL(URL.createObjectURL(i))
+      const img = new Image()
+      img.onload = () => {
+        console.log(img.width)
+      }
+      img.src = await URL.createObjectURL(i)
     }
   }
 
   const uploadToServer = async (event) => {
     const body = new FormData()
-    // console.log("file", image)
+
     body.append('file', image)
     const response = await fetch('/api/upload', {
       method: 'POST',
@@ -26,13 +32,14 @@ export default function PrivatePage(props) {
   return (
     <div>
       <div>
-        <img src={createObjectURL} />
+        <img src={createObjectURL} ref={imgRef} />
         <h4>Select Image</h4>
         <input type="file" name="myImage" onChange={uploadToClient} />
         <button className="btn btn-primary" type="submit" onClick={uploadToServer}>
           Send to server
         </button>
       </div>
+      <div></div>
     </div>
   )
 }
