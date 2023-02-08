ddea371de91f39084ed412f05ec812ed8f8a7182
rshtepin <shtep@list.ru>
2023-02-08 19:21:49 +0500

    work on compare

--- src/pages/products/compare/index.tsx
+++ src/pages/products/compare/index.tsx
@@ -1,12 +1,79 @@
-import { BlitzPage } from '@blitzjs/next'
+import { BlitzPage, useParam, useParams, useRouterQuery } from '@blitzjs/next'
 import { Wrap } from '@chakra-ui/react'
 import { Suspense } from 'react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+import Layout from 'src/core/layouts/Layout'
+import getProduct from 'src/products/queries/getProduct'
+import { useQuery } from '@blitzjs/rpc'
 
-const CompareProducts: BlitzPage = ({ list }) => {
+const CompareTable: any = () => {
+  let newarr: number = []
+  let dataarr = []
+  const id = useRouterQuery()
+  console.log(JSON.stringify(id))
+  const arr = JSON.parse(JSON.stringify(id))
+  arr.id.map((item) => newarr.push(item))
+
+  const [CompareProducts] = useQuery(getProduct, { id: 1 })
+  console.log(CompareProducts)
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Wrap width="85vw" spacing="16px" justify="center" align={'center'} mt={6}></Wrap>
-    </Suspense>
+    <TableContainer>
+      <Table variant="striped" colorScheme="teal">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>inches</Td>
+            <Td>millimetres (mm)</Td>
+            <Td isNumeric>25.4</Td>
+          </Tr>
+          <Tr>
+            <Td>feet</Td>
+            <Td>centimetres (cm)</Td>
+            <Td isNumeric>30.48</Td>
+          </Tr>
+          <Tr>
+            <Td>yards</Td>
+            <Td>metres (m)</Td>
+            <Td isNumeric>0.91444</Td>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric></Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
   )
 }
+
+const CompareProducts: BlitzPage = () => {
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CompareTable />
+      </Suspense>
+    </>
+  )
+}
+CompareProducts.getLayout = (page) => <Layout>{page}</Layout>
 export default CompareProducts
--- src/products/components/AllProducts.tsx
+++ src/products/components/AllProducts.tsx
@@ -3,7 +3,7 @@ import { Suspense, useState } from 'react'
 import getProducts from '../queries/getProducts'
 import { usePagination } from 'src/core/hooks/usePagination'
 import ProductItem from './ProductItem'
-import { Box, Wrap, WrapItem } from '@chakra-ui/react'
+import { Box, Wrap } from '@chakra-ui/react'
 import { useSession } from '@blitzjs/auth'
 import { Button } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
@@ -15,27 +15,17 @@ import { Routes } from '@blitzjs/next'
 
 const ITEMS_PER_PAGE = 30
 
-const GetProductsDB = () => {
+const GetProductsDB = ({ compare }) => {
   const [delProductMutation] = useMutation(delProduct)
 
   const router = useRouter()
   const pagination = usePagination()
-  const [toCompare, setToCompare] = useState<any>([])
+
   const [{ products, hasMore }] = usePaginatedQuery(getProducts, {
     orderBy: { order: 'asc' },
     skip: ITEMS_PER_PAGE * pagination.page,
     take: ITEMS_PER_PAGE,
   })
-
-  const compare = async ({ id, flag }) => {
-    console.log('id: ' + id)
-    console.log('flag: ' + flag)
-    flag
-      ? await setToCompare((prev) => [...prev, id])
-      : await setToCompare(toCompare.filter((item) => item !== id))
-
-    console.log(toCompare)
-  }
 
   //const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
   //const goToNextPage = () => router.push({ query: { page: page + 1 } })
@@ -53,7 +43,6 @@ const AdminBlock: any = () => {
   const role = session.role
   const [show, setShow] = useState(false)
   const [addProductMutation] = useMutation(addUpdateProduct)
-  const [compareArr, setCompareArr] = useState([])
   const onHide = () => {
     setShow(false)
   }
@@ -85,13 +74,26 @@ const AdminBlock: any = () => {
 ///////////////////
 
 const AllProducts = () => {
+  const [toCompare, setToCompare] = useState<any>([])
+  const compare = async ({ id, flag }) => {
+    console.log('id: ' + id)
+    console.log('flag: ' + flag)
+    flag
+      ? await setToCompare((prev) => [...prev, id])
+      : await setToCompare(toCompare.filter((item) => item !== id))
+  }
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Wrap width="85vw" spacing="16px" justify="center" align={'center'} mt={6}>
-        <GetProductsDB />
+        <GetProductsDB compare={compare} />
       </Wrap>
-      <Button mt={5}>Сравнить</Button>
-
+      <Link href={Routes.CompareProducts({ id: toCompare })}>
+        <Button isDisabled={!(toCompare.length > 1)} mt={5}>
+          Сравнить
+        </Button>
+      </Link>
+      <div>{JSON.stringify(toCompare)}</div>
       <AdminBlock />
     </Suspense>
   )
