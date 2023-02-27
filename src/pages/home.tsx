f3ab0774c972f2717fe22b4267e2235f8a68fc43
rshtepin <shtep@list.ru>
2023-02-27 19:20:14 +0500

    Email work

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,6 @@
 import { Routes, BlitzPage } from '@blitzjs/next'
-import Layout from 'src/core/layouts/Layout'
 import logout from 'src/auth/mutations/logout'
 import { useMutation } from '@blitzjs/rpc'
-
-import { Button } from '@chakra-ui/react'
 import HomeComponent from 'src/home/components/HomeComponent'
 const Home: BlitzPage = () => {
   const [logoutMutation] = useMutation(logout)
