91f05e5675a8801bf5c0a1c1819a2ff9fc51e12b
rshtepin <shtep@list.ru>
2023-03-10 16:07:29 +0500

    wip

--- src/products/components/ProductItemCard.tsx
+++ src/products/components/ProductItemCard.tsx
@@ -35,7 +35,7 @@ const AdminBlock: any = ({ product, onDelete }: ProductItemCardProps) => {
       <>
         <Grid mt={1} templateColumns="repeat(5, 1fr)" gap={1}>
           <GridItem>
-            <Link href={Routes.EditProductPage({ productId: product.id })}>
+            <Link href={Routes.EditProductPage({ productId: product.id.toString() })}>
               <Button mr={1} colorScheme="yellow">
                 Редактировать
               </Button>
